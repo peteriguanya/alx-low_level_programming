6ce7ae136967ca29d0727a9ed17fee6a05670ab6
peteriguanya <iguanyapeter7@gmail.com>
2022-09-11 10:09:03 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,12 @@ int main(void)
 	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
-
-	        if (c != 57)
+                if (c != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-
-	return (0);
+        return (0);
 }
-       
