e6ea32162c5f2c7243ef2233e525307b5eb1ebf9
peteriguanya <iguanyapeter7@gmail.com>
2022-09-30 16:58:49 -0700

    Task 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it recieves
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int s;
+
+	for (s = 0; s < argc; s++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
