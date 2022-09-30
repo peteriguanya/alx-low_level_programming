d56098bfc0c67532a3c3b50853a19989125141bd
peteriguanya <iguanyapeter7@gmail.com>
2022-09-30 16:39:26 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"a
+#include <stdio.h>
+
+/**
+ * main - Prints name of program followed by new line
+ * @argc: argument to be counted
+ * @argv: argument vector
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
