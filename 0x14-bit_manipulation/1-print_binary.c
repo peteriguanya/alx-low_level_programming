664889e5269a0e7cafc9b763e0832b84018cc267
peteriguanya <iguanyapeter7@gmail.com>
2022-10-31 13:04:37 -0700

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - function that prints binary representation of a number
+ * @n: unsigned long  int type;
+ *
+ * Return: always successful
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int check;
+	unsigned int size = sizeof(n) * 8;
+	unsigned int count;
+	unsigned int flag = 1;
+
+	count = 0;
+	while (count < size)
+	{
+		check = (n << 1);
+		check >>= 1;
+		if (n != check)
+		{
+			flag = 0;
+			write(1, "1", 1);
+		}
+		else if (!flag)
+		{
+			write(1, "0", 1);
+		}
+		n <<=  1;
+		count++;
+	}
+	if (flag == 1)
+		write(1, "0", 1);
+}
