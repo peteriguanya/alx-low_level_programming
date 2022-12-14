112703bcbd945fcea9e6dcac56ccd6a801cad1ec
peteriguanya <iguanyapeter7@gmail.com>
2022-12-14 08:25:30 -0800

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.peteriguanyacontent.com/peteriguanya/alx-low_level_programming/master/0x18-dynamic_libraries/libdynamic.so
-export LD_PRELOAD="$PWD/../libdynamic.so"
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
